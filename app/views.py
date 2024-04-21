ca9b11f5485438f6083bdcbe5695136c82e70dd7
Ying Tong <yingtong0106@gmail.com>
2024-04-21 23:56:03 +0800

    Added additional comments

--- app/views.py
+++ app/views.py
@@ -6,7 +6,7 @@ from flask import Blueprint, render_template
 # Create new flask Blueprint with name "views"
 views = Blueprint("views",__name__)
 
-# Define route for home page (/home)
+# NOTE: Incomplete! 
 @views.route("/home")
 def home():
     return render_template("base.html")
