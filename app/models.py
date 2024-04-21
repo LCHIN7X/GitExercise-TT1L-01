cff60d54c2b9e4758c10e60c64e0563b3a9585a8
Ying Tong <yingtong0106@gmail.com>
2024-04-21 21:40:42 +0800

    added student_id column in User table

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from . import database as db
 
 class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(200),unique=True)
-    username = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
+    email = db.Column(db.String(200), unique=True, nullable=False)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    student_id = db.Column(db.String(10), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
