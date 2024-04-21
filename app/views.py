aa2181af37b9f399441ea3e071781a5a0e2363d6
Ying Tong <yingtong0106@gmail.com>
2024-04-21 11:01:58 +0800

    created create_account.html and declared new route for creating accounts

--- app/views.py
+++ app/views.py
@@ -5,3 +5,7 @@ views = Blueprint("views",__name__)
 @views.route("/home")
 def home():
     return render_template("base.html")
+
+@views.route("/create_account")
+def create_account():
+    return render_template("create_account.html")
