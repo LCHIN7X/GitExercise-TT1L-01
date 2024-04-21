b1aafa32b465f28bbe919074ee67917e2e8625f5
Ying Tong <yingtong0106@gmail.com>
2024-04-21 10:34:30 +0800

    defined User table

---
+++ app/models.py
@@ -0,0 +1,7 @@
+from . import database as db 
+
+class User(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(200),unique=True)
+    username = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
