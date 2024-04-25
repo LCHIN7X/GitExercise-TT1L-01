45f04e54a30f76bebe711a3bb2e6b7fe27f92b05
Ying Tong <yingtong0106@gmail.com>
2024-04-25 16:25:53 +0800

    Added email validation and username checking for account creation, make flashed messages display on the screen

--- app/auth.py
+++ app/auth.py
@@ -3,6 +3,9 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from . import database as db
 from werkzeug.security import generate_password_hash
+from email_validator import validate_email, EmailNotValidError
+from sqlalchemy import and_
+from sqlalchemy.exc import IntegrityError
 
 # ------------------------------- CODE ---------------------------------------------
 
@@ -22,10 +25,15 @@ def create_account():
         password2 = request.form.get("password2")
 
         #  check if credentials meet the program requirements
-        if len(email) < 5:
-            flash("Please enter a valid email address.",category="error")
 
-        elif len(username) < 2:
+        try:
+            email_info = validate_email(email,check_deliverability=False)
+            normalized_email = email_info.normalized 
+
+        except EmailNotValidError as e:
+            flash("Invalid email address.",category="error")
+
+        if len(username) < 2:
             flash("Username must be at least 2 characters long.",category="error")
 
         elif len(student_id) != 10:
@@ -37,23 +45,29 @@ def create_account():
         elif password1 != password2:
             flash("Passwords do not match.",category="error")
         
-        else:
-            # if credentials meet the requirements, first check if user already has an account.
-            user_in_db = User.query.filter_by(email=email).first()
+        else:            
+            user_in_db = User.query.filter(and_(User.email == normalized_email,
+                                                User.student_id == student_id)).first()
 
-            # if user record is not in database, create new account
-            if not user_in_db:
-                new_user_account = User(email=email,
-                                    username=username,
-                                    student_id=student_id,
-                                    password=generate_password_hash(password1,method="scrypt"))
-                db.session.add(new_user_account)
-                db.session.commit()
-                return redirect(url_for("views.home"))
+            if user_in_db:
+                flash("Account already exists",category="error")
 
-            # if user does have an account, show error message
-            else:
-                flash("Account already exists.",category="error")
+            else: 
+            # if user record is not in database, try to create new account
+                try:
+                    new_user_account = User(email=normalized_email,
+                                            username=username,
+                                            student_id=student_id,
+                                            password=generate_password_hash(password1,method="scrypt"))
+                    db.session.add(new_user_account)
+                    db.session.commit()
+                    flash("Account successfully created!",category="success")
+                    return redirect(url_for("views.home"))
+                
+                except IntegrityError:
+                    db.session.rollback()
+                    flash("Username already taken, please enter a new username.",category="error")
+
     
     # if method is GET, render page
     return render_template("create_account.html")
