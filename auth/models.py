f4ef7f77656a6e5f84e316cc66fb0877532fdd03
Ying Tong <yingtong0106@gmail.com>
2024-04-26 11:17:41 +0800

    Changed folder structure of project (each folder represents a different aspect of the project)

---
+++ auth/models.py
@@ -0,0 +1,12 @@
+# imports
+from main import database as db 
+
+# ------------------------------- CODE ---------------------------------------------
+
+# Create User table
+class User(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(200), unique=True, nullable=False)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    student_id = db.Column(db.String(10), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
--- main.py
+++ main.py
@@ -1,4 +1,28 @@
-from app import create_app 
+from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy 
+
+# ------------------------------- CODE ---------------------------------------------
+
+# Intialize SQLAlchemy database
+database = SQLAlchemy()
+DATABASE_NAME = "database.db"
+
+# Create function to create app instance
+def create_app():
+    # create new Flask app and configuring app settings
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = "wellofwisdom"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DATABASE_NAME}"
+    database.init_app(app)
+
+    #  registering flask Blueprints
+    from auth.views import auth
+    app.register_blueprint(auth, url_prefix="/auth")
+
+    with app.app_context():
+        database.create_all()
+
+    return app 
 
 if __name__ == "__main__":
     app = create_app()
