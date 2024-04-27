50acb4805efa110445ea54164cbd32ddfab981ac
Le Zi Rui <zirui40103@gmail.com>
2024-04-27 20:58:46 +0800

    update

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask 
-from flask_sqlalchemy import SQLAlchemy 
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 from auth.models import db
-
-# ------------------------------- CODE ---------------------------------------------
+from auth.views import auth
+from app.views import home, add
 
 # Intialize SQLAlchemy database
 DATABASE_NAME = "database.db"
@@ -15,15 +15,26 @@ def create_app():
     app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DATABASE_NAME}"
     db.init_app(app)
 
-    #  registering flask Blueprints
-    from auth.views import auth
+    # Registering flask Blueprints
     app.register_blueprint(auth, url_prefix="/auth")
 
     with app.app_context():
         db.create_all()
 
-    return app 
+    return app
 
-if __name__ == "__main__":
-    app = create_app()
+# Get the Flask application instance
+app = create_app()
+
+# Define route to render home template
+@app.route('/')
+def render_home():
+    return render_template("base.html")
+
+# Define route to render add template
+@app.route('/add')
+def render_add():
+    return render_template("add.html")
+
+if __name__ == "_main_":
     app.run(debug=True)
