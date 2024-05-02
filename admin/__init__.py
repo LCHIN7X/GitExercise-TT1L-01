74c9bcfdc5f06ca6434d4f06976725b1604eada0
Ying Tong <yingtong0106@gmail.com>
2024-05-02 22:59:14 +0800

    Added ModelView for admin page

--- admin/__init__.py
+++ admin/__init__.py
@@ -1,4 +1,12 @@
 from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from auth.models import db, User
+from books.models import Addbook, Subject
 
 #  initialize Admin instance
 admin = Admin(template_mode='bootstrap4')
+
+#  add views for admin
+admin.add_view(ModelView(User,db.session))
+admin.add_view(ModelView(Addbook,db.session))
+admin.add_view(ModelView(Subject,db.session))
