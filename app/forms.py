db5c3c3a40308ff81ee17d227ef6812a3f1c2b5b
Le Zi Rui <zirui40103@gmail.com>
2024-05-05 16:29:32 +0800

    update-and-remove

--- app/forms.py
+++ app/forms.py
@@ -1,15 +1,18 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed, FileRequired
-from wtforms import StringField, IntegerField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, IntegerField, SubmitField, FileField, TextAreaField, SelectField
+from wtforms.validators import DataRequired, NumberRange
 
 
 class AddBookForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     faculty = StringField('Faculty', validators=[DataRequired()])
     subject = StringField('Subject', validators=[DataRequired()])
-    quantity = IntegerField('Quantity', validators=[DataRequired()])
-    price = IntegerField('Price', validators=[DataRequired()])
-    photo = FileField('Photo', validators=[FileRequired(), FileAllowed(['jpg', 'png'], 'Images only!')])
+    quantity = IntegerField('Quantity', validators=[DataRequired(), NumberRange(min=1)])
+    price = IntegerField('Price', validators=[DataRequired(), NumberRange(min=0)])
+    photo = FileField('Photo', validators=[DataRequired()])
     remark = TextAreaField('Remark')
-    submit = SubmitField('Add Book')
+    submit_add = SubmitField('Add Book')
+
+class RemoveBookForm(FlaskForm):
+    books = SelectField('Select book to remove:', validators=[DataRequired()], coerce=int)
+    submit_remove = SubmitField('Remove Book')
--- app/views.py
+++ app/views.py
@@ -1,11 +1,11 @@
 ### from flask import Blueprint, render_template
-from flask import Blueprint, render_template, url_for,request,flash,redirect
+from flask import Blueprint, render_template, url_for,request,redirect
 import os
 from flask_sqlalchemy import SQLAlchemy
 from auth.models import User, db
 from flask import session
 from .models import Faculty, Subject, Quantity, Price, Books, db
-from .forms import AddBookForm
+from .forms import AddBookForm,RemoveBookForm
 from flask_uploads import UploadSet, IMAGES
 
 
@@ -29,57 +29,62 @@ photos = UploadSet('photos', IMAGES)
 
 @views.route("/add", methods=["GET", "POST"])
 def add():
-    form = AddBookForm()
+    add_form = AddBookForm()
+    remove_form = RemoveBookForm()
+    if request.method == "POST":
+        if 'submit_add' in request.form and add_form.validate_on_submit():
+            username = add_form.username.data
+            faculty_name = add_form.faculty.data
+            subject_name = add_form.subject.data
+            quantity_value = add_form.quantity.data
+            price_value = add_form.price.data
+            remark = add_form.remark.data
 
-    if request.method == "POST" and form.validate_on_submit():
-        current_username = session.get('username')
-        username = form.username.data
-        if current_username != username:
-            return render_template("add-and-remove.html", username=current_username, error="Username does not match logged in user")
+            faculty = Faculty.query.filter_by(name=faculty_name).first()
+            if not faculty:
+                faculty = Faculty(name=faculty_name)
+                db.session.add(faculty)
 
-        faculty_name = form.faculty.data
-        subject_name = form.subject.data
-        quantity_value = form.quantity.data
-        price_value = form.price.data
-        remark = form.remark.data
+            subject = Subject.query.filter_by(name=subject_name).first()
+            if not subject:
+                subject = Subject(name=subject_name)
+                db.session.add(subject)
 
-        faculty = Faculty.query.filter_by(name=faculty_name).first()
-        if not faculty:
-            faculty = Faculty(name=faculty_name)
-            db.session.add(faculty)
+            quantity = Quantity(value=quantity_value)
+            db.session.add(quantity)
+
+            price = Price(value=price_value)
+            db.session.add(price)
+
+            photo_filename = 'photos/' + add_form.photo.data.filename
+            add_form.photo.data.save(photo_filename)
+
+            book = Books(
+                username=username,
+                faculty_id=faculty.id,
+                subject_id=subject.id,
+                quantity_id=quantity.id,
+                price_id=price.id,
+                photo=photo_filename,
+                remark=remark
+            )
+            db.session.add(book)
             db.session.commit()
 
-        subject = Subject.query.filter_by(name=subject_name).first()
-        if not subject:
-            subject = Subject(name=subject_name)
-            db.session.add(subject)
-            db.session.commit()
+            return redirect(url_for('upload_form'))
 
-        quantity = Quantity(value=quantity_value)
-        db.session.add(quantity)
-        db.session.commit()
+        elif 'submit_remove' in request.form and remove_form.validate_on_submit():
+            book_id_to_remove = remove_form.books.data
+            book_to_remove = Books.query.get(book_id_to_remove)
+            if book_to_remove:
+                db.session.delete(book_to_remove)
+                db.session.commit()
 
-        price = Price(value=price_value)
-        db.session.add(price)
-        db.session.commit()
+            return redirect(url_for('upload_form'))
 
-        photo_filename = photos.save(form.photo.data)
+    remove_form.books.choices = [(book.id, book.subject.name) for book in Books.query.all()]
 
-        book = Books(
-            username=username,
-            faculty_id=faculty.id,
-            subject_id=subject.id,
-            quantity_id=quantity.id,
-            price_id=price.id,
-            photo=photo_filename,
-            remark=remark
-        )
-        db.session.add(book)
-        db.session.commit()
-
-        return redirect(url_for('views.upload_form'))
-
-    return render_template("add-and-remove.html", form=form)
+    return render_template("add-and-remove.html", add_form=add_form, remove_form=remove_form)
 
 @views.route("/upload_form", methods=['GET', 'POST'])
 def upload_form():
