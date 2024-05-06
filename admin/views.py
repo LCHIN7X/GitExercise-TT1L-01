cc44e5ce6320bde8b03df39118c7949f01d639cf
Ying Tong <yingtong0106@gmail.com>
2024-05-06 21:39:33 +0800

    Add code to prevent admins from deleting admin accounts

--- admin/views.py
+++ admin/views.py
@@ -1,6 +1,7 @@
 from flask_login import current_user
 from flask_admin import AdminIndexView
 from flask_admin.contrib.sqla import ModelView
+from flask import flash
 
 
 class AdminIndex(AdminIndexView):
@@ -37,7 +38,19 @@ class AdminBookView(AdminModelView):
 
 
 class AdminUserView(AdminModelView):
-    column_list = ['id','email','username','student_id']
-    form_columns = ['email','username','student_id','password']
-    column_filters = ['email','username','student_id']
-    column_searchable_list = ['email','username','student_id']
+    column_list = ['id','email','username','student_id','is_admin']
+    form_columns = ['email','username','student_id','password','is_admin']
+    column_filters = ['email','username','student_id','is_admin']
+    column_searchable_list = ['email','username','student_id','is_admin']
+
+
+    def delete_model(self, model):
+        if current_user == model:
+            flash("You Cannot Delete Accounts That Are Active In Use.",category="error")
+            return False
+
+        if model.is_admin:
+            flash("You Cannot Delete Admin Accounts.",category="error")
+            return False
+
+        return super().delete_model(model)
