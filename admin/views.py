a7069f8360c5690f98d0047cd662766faf261999
Ying Tong <yingtong0106@gmail.com>
2024-05-21 21:06:57 +0800

    Fix Book and Invoice view

--- admin/views.py
+++ admin/views.py
@@ -7,12 +7,12 @@ from flask_admin.form.upload import FileUploadField
 from wtforms.validators import InputRequired, ValidationError
 from werkzeug.utils import secure_filename
 from werkzeug.security import generate_password_hash
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import sessionmaker
 
 
 def file_is_valid(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'jpg', 'png', 'jpeg'}
-
-
 
 class AdminIndex(AdminIndexView):
     def is_accessible(self):
@@ -23,75 +23,86 @@ class AdminIndex(AdminIndexView):
     
     def can_delete(self):
         return True
-    
-
 
 class AdminModelView(ModelView):
     def is_accessible(self):
         return current_user.is_authenticated and current_user.is_admin
     
     def can_edit(self):
-        return True 
+        return True
     
     def can_delete(self):
         return True
     
     def can_create(self):
         return True
-    
 
 class AdminBookView(AdminModelView):
     column_labels = {
-        "name" : "Book Name",
-        "faculty.name" : "Faculty",
-        "subject.name" : "Subject",
-        "desc" : "Description",
-        "pub_date" : "Date Published"
+        "name": "Book Name",
+        "faculty.name": "Faculty",
+        "subject.name": "Subject",
+        "desc": "Description",
+        "pub_date": "Date Published",
+        'con' : 'Condition'
     }
-    column_list = ['name','price','stock','desc','pub_date','faculty','subject']
-    form_columns = ['name','price','stock','desc','pub_date','faculty','subject','image','user']
-    column_filters = ['name','faculty.name','subject.name','pub_date']
-    column_sortable_list = ['name','price','stock','pub_date', ('faculty','faculty.name'), ('subject', 'subject.name')]
+    column_list = ['name', 'price', 'stock', 'desc', 'con', 'pub_date', 'faculty', 'subject']
+    form_columns = ['name', 'price', 'stock', 'desc','con','pub_date', 'faculty', 'subject', 'image','user']
+    column_filters = ['name', 'faculty.name', 'subject.name', 'pub_date']
+    column_sortable_list = ['name', 'price', 'stock', 'pub_date', ('faculty', 'faculty.name'), ('subject', 'subject.name')]
     column_searchable_list = ['name']
 
     form_extra_fields = {
-        'image' : FileUploadField('Image',
-                                  base_path="static/images", 
-                                  relative_path="assets/images/",
-                                  validators=[InputRequired()])
-                        }
+        'image': FileUploadField('Image',
+                                 base_path="static/images",
+                                 relative_path="assets/images/",
+                                 validators=[InputRequired()])
+    }
 
-    
-    def on_model_change(self, model, form, is_created):
+    form_args = {
+        'con' : {
+            'default' : 'Brand New'
+        }
+    }
+
+    def on_model_change(self, form, model, is_created):
         if is_created:
-            model.user_id = current_user.id 
-        model.user = current_user
+            # Check if a book with the same name already exists
+            book_already_exists = Book.query.filter_by(name=model.name).first()
+            if book_already_exists:
+                model.is_original = False
+            else:
+                model.is_original = True
 
-    
+            model.user_id = current_user.id
+
+        model.user = current_user
+        super().on_model_change(form, model, is_created)
+
+
     def validate_form(self, form):
-        
-        img_field = form.image
-        if img_field.data:
-            filename = secure_filename(img_field.data.filename)
-            if not file_is_valid(filename):
-                raise ValidationError('Invalid File Type: Only .jpg, .png and .jpeg Are Allowed.')
-            
-        return super().validate_form(form)    
+        if hasattr(form, 'image'):
+            img_field = form.image
+            if img_field.data and hasattr(img_field.data, 'filename'):
+                filename = secure_filename(img_field.data.filename)
+                if not file_is_valid(filename):
+                    raise ValidationError('Invalid File Type: Only .jpg, .png and .jpeg Are Allowed.')
+
+        return super().validate_form(form)
 
     def on_validation_error(self, form):
-        flash('Form validation failed',category='error')
+        flash('Form validation failed', category='error')
         return redirect(url_for('admin.index'))
-
 
 class AdminUserView(AdminModelView):
     column_labels = {
-        'email' : "Email",
-        "username" : "Username",
-        "student_id" : 'Student ID',
-        "is_admin" : "Is Admin"
+        'email': "Email",
+        "username": "Username",
+        "student_id": 'Student ID',
+        "is_admin": "Is Admin"
     }
-    column_list = ['email','username','student_id','is_admin']
-    form_columns = ['email','username','student_id','password','is_admin']
+    column_list = ['email', 'username', 'student_id', 'is_admin']
+    form_columns = ['email', 'username', 'student_id', 'password', 'is_admin']
     form_edit_rules = [
         'email',
         'username',
@@ -105,42 +116,39 @@ class AdminUserView(AdminModelView):
         'password',
         'is_admin'
     ]
-    column_filters = ['email','username','student_id','is_admin']
-    column_searchable_list = ['email','username','student_id','is_admin']
-
+    column_filters = ['email', 'username', 'student_id', 'is_admin']
+    column_searchable_list = ['email', 'username', 'student_id', 'is_admin']
 
     def create_model(self, form):
         password = form.password.data
-        hashed_password = generate_password_hash(password,method="scrypt")
+        hashed_password = generate_password_hash(password, method="scrypt")
         form.password.data = hashed_password
 
         return super().create_model(form)
 
-
     def delete_model(self, model):
         if current_user == model:
-            flash("You Cannot Delete Accounts That Are Active In Use.",category="error")
+            flash("You Cannot Delete Accounts That Are Active In Use.", category="error")
             return False
 
         if model.is_admin:
-            flash("You Cannot Delete Admin Accounts.",category="error")
+            flash("You Cannot Delete Admin Accounts.", category="error")
             return False
 
         return super().delete_model(model)
-    
 
 class AdminInvoiceView(AdminModelView):
     column_labels = {
-        "id" : "ID",
-        "invoice" : "Invoice Details",
-        "status" : "Status",
-        "data_order" : "Date Ordered"
+        "id": "ID",
+        "invoice": "Invoice Details",
+        "status": "Status",
+        "data_order": "Date Ordered"
     }
-    column_list = ['invoice','status','data_order']
-    form_columns = ['invoice','status']
-    column_filters = ['status','invoice','data_order']
-    column_searchable_list = ['status','invoice']
-    column_sortable_list = ['status','invoice','data_order']
+    column_list = ['user', 'invoice', 'status', 'data_order']
+    form_columns = ['invoice', 'status']
+    column_filters = ['status', 'invoice', 'data_order', 'user']
+    column_searchable_list = ['status', 'invoice', 'user']
+    column_sortable_list = ['status', 'invoice', 'data_order', 'user']
 
     form_edit_rules = [
         "status"
