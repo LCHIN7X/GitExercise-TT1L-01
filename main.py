ce53da3c7f4519b71dc7e6817e4cd3976c796798
Ying Tong <yingtong0106@gmail.com>
2024-06-02 16:38:57 +0800

    Reformat main.py

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_uploads import IMAGES, UploadSet, configure_uploads
 from flask_login import LoginManager
 from auth.models import db
 from flask_msearch import Search
-
 import os
-
-# ------------------------------- CODE ---------------------------------------------
-# Intialize SQLAlchemy database
-
-
-# search = Search()
 from auth.views import auth
 from admin import admin, add_admin_to_db
 
@@ -28,20 +20,17 @@ photos = UploadSet("photos", IMAGES)
 def create_app():
     # create new Flask app and configuring app settings
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = os.urandom(24)
     app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DATABASE_NAME}"
     app.config["UPLOADED_PHOTOS_DEST"] = "static/images"
     app.config["SECRET_KEY"] = os.urandom(24)
    
-    
     configure_uploads(app, photos)
-    search.init_app(app)
-
+    
     from auth.models import User
     
     db.init_app(app)
     admin.init_app(app)
-
+    search.init_app(app)
 
     #  registering flask Blueprints
     from auth.views import auth
@@ -54,19 +43,15 @@ def create_app():
     app.register_blueprint(user,url_prefix="/user",name="user_bp")
 
     
-    from books.models import Faculty, Subject, Book
     from shbooks.views import shbooks 
     
     app.register_blueprint(shbooks,url_prefix="/shbooks")
 
   
-    
-
     with app.app_context():
         db.create_all()
     
     
-    from auth.models import User
     add_admin_to_db(app)
     
     # create LoginManager object to handle logins
@@ -75,16 +60,13 @@ def create_app():
     login_manager.init_app(app)
 
 
-   
     @login_manager.user_loader 
     def load_user(id):
         user = db.session.get(User, int(id))
-        print(user)
         return user
     
     return app 
 
-# Get the Flask application instance
 
 
 if __name__ == "__main__":
